b3c9fc97f44f069c989a752bce690a091f78fe84
maomaoovo <maomaoovo@outlook.com>
2025-09-15 23:22:37 +0800

    chore: 修正提交-补充缓存文件

--- engine/engine_timer.c
+++ engine/engine_timer.c
@@ -1,1 +1,23 @@
 #include "engine_timer.h"
+
+static LARGE_INTEGER freq, last_tick, now_tick;
+static float delta_time = 0.0f;
+static float total_time = 0.0f;
+
+void EngineTimer_Init()
+{
+	QueryPerformanceFrequency(&freq);
+	QueryPerformanceCounter(&last_tick);
+	total_time = 0.0f;
+}
+
+void EngineTimer_Tick()
+{
+	QueryPerformanceCounter(&now_tick);
+	delta_time = (float)(now_tick.QuadPart - last_tick.QuadPart) / freq.QuadPart;
+	total_time += delta_time;
+	last_tick = now_tick;
+}
+
+float EngineTimer_GetDeltaTime() { return delta_time; }
+float EngineTimer_GetTime() { return total_time; }
