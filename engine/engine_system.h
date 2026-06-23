7d5e4675f266e7bdc4485caff6c6360796ccd056
Asleepdoggy <maomaoovo@outlook.com>
2026-06-23 14:27:39 +0800

    fix: 修复头文件自包含的问题

--- engine/engine_system.h
+++ engine/engine_system.h
@@ -1,11 +1,8 @@
-//集成所有底层系统头文件的头文件
-
 #pragma once
 
 #include "engine_input.h"
 #include "engine_render.h"
 #include "engine_resource.h"
-#include "engine_system.h"
 #include "engine_timer.h"
 #include "engine_window.h"
 #include "engine_game.h"
