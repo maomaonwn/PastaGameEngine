7192069f7b3cb6bc43ed05cd4f50a229bff43739
Asleepdoggy <maomaoovo@outlook.com>
2026-06-23 14:30:49 +0800

    feat: 聚合用户可用的接口，扩展为统一的用户 API 头文件
    聚合了 Debug、Render、Resource、Timer、Input 所有用户可用的接口

--- engine/engine_api.h
+++ engine/engine_api.h
@@ -1,17 +1,38 @@
 #pragma once
 
+#include <windows.h>
+
 #ifdef _cplusplus
 extern "C" {
 #endif
 
+// Debug
 typedef enum {
-		LOGWARN,
-		LOGERROR,
-		LOGINFO
-}ExType;
+	LOGWARN,
+	LOGERROR,
+	LOGINFO
+} ExType;
 
 void DebugLog(const char* msg);
-void DebugLogEx(ExType type, const char* fmt);
+void DebugLogEx(ExType type, const char* fmt, ...);
+
+// Render
+void EngineRender_Clear(COLORREF color);
+void EngineRender_DrawRect(int x, int y, int w, int h, COLORREF color);
+void EngineRender_DrawRectOutline(int x, int y, int w, int h, COLORREF color);
+void EngineRender_DrawBitmap(HBITMAP hBitmap, int x, int y, int w, int h);
+void EngineRender_DrawText(const char* text, int x, int y, COLORREF color);
+
+// Resource
+HBITMAP EngineResource_LoadBitmap(const char* filename);
+void EngineResource_FreeBitmap(HBITMAP hBitmap);
+
+// Timer
+float EngineTimer_GetDeltaTime(void);
+float EngineTimer_GetTime(void);
+
+// Input
+BOOL EngineInput_IsKeyDown(int vk_code);
 
 #ifdef _cplusplus
 }
